1854f53a58ed28c0baa38de946645fb486fc4cc6
LaurentCOLL1 <128934707+LaurentCOLL1@users.noreply.github.com>
2026-08-01 16:27:10 +0200

    feat: exporter l'atelier avancé

--- src/assistant_botanique/ui/v3_tabs.py
+++ src/assistant_botanique/ui/v3_tabs.py
@@ -1,5 +1,6 @@
 """Exports des onglets de la version 3."""
 from .adaptive_tab import AdaptiveCareTab
+from .advanced_ecosystem_tab import AdvancedEcosystemTab
 from .maintenance_tab import MaintenanceTab
 from .photo_tab import PhotoTimelineTab
 from .productivity_tabs import CareCalendarTab, GlobalSearchTab, TodayDashboardTab
@@ -7,6 +8,7 @@ from .review_tab import CatalogueReviewTab
 
 __all__ = [
     "AdaptiveCareTab",
+    "AdvancedEcosystemTab",
     "CareCalendarTab",
     "CatalogueReviewTab",
     "GlobalSearchTab",
