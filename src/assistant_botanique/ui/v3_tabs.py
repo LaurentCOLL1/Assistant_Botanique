b2dd769fdfc318378824ac63b5f2a9cba43ddc8e
LaurentCOLL1 <128934707+LaurentCOLL1@users.noreply.github.com>
2026-08-01 11:46:34 +0200

    feat: exposer les nouveaux onglets

--- src/assistant_botanique/ui/v3_tabs.py
+++ src/assistant_botanique/ui/v3_tabs.py
@@ -2,6 +2,15 @@
 from .adaptive_tab import AdaptiveCareTab
 from .maintenance_tab import MaintenanceTab
 from .photo_tab import PhotoTimelineTab
+from .productivity_tabs import CareCalendarTab, GlobalSearchTab, TodayDashboardTab
 from .review_tab import CatalogueReviewTab
 
-__all__ = ["AdaptiveCareTab", "CatalogueReviewTab", "MaintenanceTab", "PhotoTimelineTab"]
+__all__ = [
+    "AdaptiveCareTab",
+    "CareCalendarTab",
+    "CatalogueReviewTab",
+    "GlobalSearchTab",
+    "MaintenanceTab",
+    "PhotoTimelineTab",
+    "TodayDashboardTab",
+]
